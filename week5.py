ce8deef98289ce398030842cc1e7498527449f4c
Feniuk <143640539+Feniuk@users.noreply.github.com>
2025-11-12 21:58:55 +0100

    date of birth finished

--- week5.py
+++ week5.py
@@ -2,13 +2,15 @@
 
 # mymodule.calculator()
 # mymodule.module_name
-
 import datetime
+from datetime import datetime
+from datetime import date 
+
 def date_calculation():
-    person_year_of_birth = str(input("What's your date of birth? "))
-    person_year_of_birth 
-    current_year = datetime.datetime.now().year
-    years_old = current_year - person_year_of_birth
+    date_of_birth_str = input("Enter your date of birth (YYYY-MM-DD: ")
+    date_of_birth = datetime.strptime(date_of_birth_str,"%Y-%m-%d").date()
+    current_date = date.today()
+    years_old = current_date.year - date_of_birth.year
     print("You are: ", years_old, "years old")
 
 date_calculation()
