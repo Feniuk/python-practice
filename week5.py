c254a159d8f9bfbae75972bd8bf7f9381d4bed15
Feniuk <143640539+Feniuk@users.noreply.github.com>
2025-12-14 21:38:50 +0100

    python classes practice

--- week5.py
+++ week5.py
@@ -15,3 +15,42 @@ def date_calculation():
 
 date_calculation()
 
+
+class University:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+    def __str__(self):
+        return f"University: {self.name} ,students: {len(self.students)}"
+    def register_student(self, student):
+        self.students.append(student)
+    def graduate_student(self, student):
+        self.students.pop(student)
+
+class TodoList:
+    def __init__(self, owner):
+        self.owner = owner
+        self.tasks = []
+
+    def add_task(self):
+        user_task1 = input("Enter a task: ")
+        user_task2 = input("Enter a second task: ")
+        self.tasks.append({"task": user_task1, "done": False})
+        self.tasks.append({"task": user_task2, "done": True})
+
+    def done(self, task_name):
+        for task in self.tasks:
+            if task["task"] == task_name:
+                if task["done"] is True:
+                    return task
+                else:
+                    return "Not done yet, loh"
+        return "Task not found"
+
+todo = TodoList("Misha")
+todo.add_task()
+print(todo.tasks)  
+print(todo.done(todo.tasks[1]["task"]))
+
+
+
